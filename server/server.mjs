5cff6c0c02ca6ec886e04c3205959455c8245600
James Y <jyab0617@gmail.com>
2023-08-15 23:55:07 -0400

    refactor imports

--- server/server.mjs
+++ server/server.mjs
@@ -1,16 +1,18 @@
 import "./loadEnvironment.mjs";
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import records from "./routes/record.mjs";
+import userRoutes from "./routes/userRoutes.mjs";
 import colors from "colors";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 app.use("/record", records);
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
     console.log(colors.yellow.bold(`Server is running on port: ${PORT}`));
