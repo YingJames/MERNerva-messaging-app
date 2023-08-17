d1bb7eb24fe26280b55aacf0d40091d694c4d67f
James Y <jyab0617@gmail.com>
2023-08-17 11:18:18 -0400

    move server to root directory

--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@ import "./loadEnvironment.mjs";
 import express, { json } from "express";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 import records from "./routes/record.mjs";
 import userRoutes from "./routes/userRoutes.mjs";
 import colors from "colors";
@@ -12,14 +13,17 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', (request, response) => {
-   response.sendFile(path.join(__dirname, 'build', 'index.html'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 app.use("/record", records);
 app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(colors.yellow.bold(`Server is running on port: ${PORT}`));
+    console.log(colors.yellow.bold(`Server is running on http://localhost:${PORT}`));
 });
