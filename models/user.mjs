ce6f5112f2324fd8ffc96035d5a3c01fad6d5ba4
YingJames <jamesy148@usf.edu>
2023-08-22 09:17:28 -0400

    export both schema and model

--- models/user.mjs
+++ models/user.mjs
@@ -1,11 +1,10 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
+export const userSchema = new Schema({
     uid: { type: String, required: true, unique: true },
     displayName: { type: String, required: true },
     email: { type: String, required: true },
     // Additional user properties can be added here
 });
 
-const User = model("User", userSchema);
-export default User;
+export const User = model("User", userSchema);
