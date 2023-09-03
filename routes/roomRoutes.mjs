caa9e886eeaff010b2bf0734f5b9faa574e8c1dd
YingJames <jamesy148@usf.edu>
2023-09-03 13:54:16 -0400

    implement check for existing document

--- routes/roomRoutes.mjs
+++ routes/roomRoutes.mjs
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Types } from "mongoose"; // or from mongoose
 import { Room } from "../models/room.mjs";
+import MessageThread from "../models/messageThread.mjs";
 import { connection } from "../config/mongoClient.mjs"
 
 const roomRoutes = Router();
@@ -16,34 +17,59 @@ roomRoutes.post('/createRoom', async (request, response) => {
         }
 
         console.log(`name: ${name}, participants: ${participants}`);
+
+        const newMessageThread = new MessageThread();
         const newRoom = new Room({
             name,
             participants,
+            messageThread: newMessageThread._id
         });
-        const messageDB = await connection.useDb("messageDB");
-        const userCollection = await messageDB.collection("users");
-        const roomCollection = await messageDB.collection("chat-rooms");
-        const parentId = new Types.ObjectId("64efd8e70068f45cae562950");
+
+        const messageDB = connection.useDb("messageDB");
+        const roomCollection = messageDB.collection("rooms");
+        const userCollection = messageDB.collection("users");
+        const messageThreadCollection = messageDB.collection("messagethreads");
 
         // update chatrooms collection
-        await roomCollection.updateOne(
-            { _id: parentId },
-            { $push: { Rooms: newRoom } }
+        const existingRoomDocu = await roomCollection.findOne();
+        if (existingRoomDocu) {
+            await roomCollection.updateOne(
+                { _id: existingRoomDocu._id },
+                { $push: { Rooms: newRoom } }
+            );
+        } else {
+            await roomCollection.insertOne({
+                Rooms: [newRoom]
+            });
+        }
+
+        await userCollection.updateOne(
+            { "Users._id": { $in: participants } },
+            { $push: { "Users.$[participant].rooms": newRoom._id } },
+            { arrayFilters: [{ "participant._id": { $in: participants } }] }
         );
 
-        await userCollection.updateMany(
-            { "Users._id": { $in: participants }},
-            { 
-                $push: { 
-                    "Users.$[].rooms": newRoom._id 
-                } 
-            }
-        );
+        // update messagethreads collection
+        const existingMessageThreadDocu = await messageThreadCollection.findOne();
+        if (existingMessageThreadDocu) {
+            await messageThreadCollection.updateOne(
+                { _id: existingMessageThreadDocu._id },
+                { $push: { MessageThreads: newMessageThread } }
+            );
+        } else {
+            await messageThreadCollection.insertOne({
+                MessageThreads: [newMessageThread]
+            });
+        }
 
-        response.status(200).json({ message: "ChatRoom created successfully" });
+        response.status(200).json({
+            message: "ChatRoom created successfully",
+            roomId: newRoom._id,
+            messageThreadId: newMessageThread._id
+        });
     } catch (error) {
         console.log(error);
-        response.status(500).json({ message: "ChatRoom could not be created" }); 
+        response.status(500).json({ message: "ChatRoom could not be created" });
     }
 });
 
