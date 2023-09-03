58fea94808745a573088d0697ee30086dc10e5b3
YingJames <jamesy148@usf.edu>
2023-09-03 13:42:35 -0400

    prevent auto creation of messages collection in mongodb

--- models/message.mjs
+++ models/message.mjs
@@ -7,7 +7,9 @@ const messageSchema = new Schema({
     // receiver: { type: ObjectId, ref: 'User', required: true },
     content: { type: String, required: true },
     timestamp: { type: Date, required: true, default: Date.now },
-});
+},
+    { autoCreate: false }
+);
 
-const Message = model("User", messageSchema);
+const Message = model("Message", messageSchema);
 export { messageSchema, Message };
