9263923e44c0a6d6596d271477e2e983d773b594
James Y <jyab0617@gmail.com>
2023-09-04 13:33:46 -0400

    add error handling

--- client/src/requests/users.js
+++ client/src/requests/users.js
@@ -1,18 +1,23 @@
 export async function CreateUser(userData) {
-    const {email, displayName, uid } = userData;
-    const response = await fetch("http://localhost:5050/api/database/users/createUser", {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "email": email,
-            "displayName": displayName,
-            "uid": uid
-        })
-    });
+    try {
+        const {email, displayName, uid } = userData;
+        const response = await fetch("http://localhost:5050/api/database/users/createUser", {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "email": email,
+                "displayName": displayName,
+                "uid": uid
+            })
+        });
 
-    return response;
+        console.log(response);
+    } catch (error) {
+        console.log("Error creating user:", error);
+        throw error;
+    }
 };
 
