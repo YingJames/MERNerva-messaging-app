9e7d2c1bbe476dd89968c04beee9b70b09f56f91
YingJames <jamesy148@usf.edu>
2023-09-06 19:11:36 -0400

    implement FindUser function abstraction in frontend

--- client/src/requests/users.js
+++ client/src/requests/users.js
@@ -1,6 +1,27 @@
+export async function FindUser(email) {
+    try {
+        const request = await fetch("http://localhost:5050/api/database/users/findUser", {
+            method: "POST",
+            cors: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "email": email
+            }),
+        });
+
+        return request;
+    } catch (error) {
+        console.log("Error finding user:", error);
+        throw error;
+     }
+
+}
+
 export async function CreateUser(userData) {
     try {
-        const {email, displayName, uid } = userData;
+        const { email, displayName, uid } = userData;
         const response = await fetch("http://localhost:5050/api/database/users/createUser", {
             method: 'POST',
             mode: 'cors',
