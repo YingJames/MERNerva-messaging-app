1110ae8fc6ee6db7c0258cf3ed26db2ea98f7935
James Y <jyab0617@gmail.com>
2023-09-18 22:20:06 -0400

    implement new api to access messages from thread

--- routes/messageRoutes.mjs
+++ routes/messageRoutes.mjs
@@ -5,9 +5,38 @@ import { connection } from "../config/mongoClient.mjs"
 
 const messageRoutes = Router();
 
+messageRoutes.post('/findMessageThread', async (request, response) => {
+    try {
+        let { messageThreadId } = await request.body;
+        if (typeof messageThreadId == "string") {
+            messageThreadId = new Types.ObjectId(messageThreadId);
+        }
+
+        const messageDB = connection.useDb("messageDB");
+        const messageThreadCollection = messageDB.collection("messagethreads");
+
+        const messageThread = await messageThreadCollection.aggregate(
+            [
+                { $match: { "MessageThreads._id": messageThreadId } },
+                { $unwind: "$MessageThreads" },
+                { $match: { "MessageThreads._id": messageThreadId } },
+                { $project: {
+                    _id: "$MessageThreads._id",
+                    messages: "$MessageThreads.messages"
+                    } }
+            ]
+        ).toArray();
+        // const messagesArray = messageThread[0].MessageThreads[0].messagesArray;
+
+        response.status(200).json({ message: "MessageThread found successfully", messageThread });
+    } catch (error) {
+        response.status(500).json({ error: "MessageThread could not be found", details: error.message });
+    }
+});
+
 messageRoutes.post('/createMessage', async (request, response) => {
     try {
-        let { messageThreadId, senderId, content  } = await request.body;
+        let { messageThreadId, senderId, content } = await request.body;
         if (typeof messageThreadId == "string") {
             messageThreadId = new Types.ObjectId(messageThreadId);
         }
@@ -25,7 +54,7 @@ messageRoutes.post('/createMessage', async (request, response) => {
         );
 
         response.status(201).json({ message: "Message created successfully", newMessage });
-    } catch(error) {
+    } catch (error) {
         response.status(500).json({ error: "Message creation failed", details: error.message });
     }
 });
