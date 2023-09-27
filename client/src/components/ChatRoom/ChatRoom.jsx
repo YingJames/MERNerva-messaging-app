8c22edac357a01e663346f90984efe229a3c3438
James Y <jyab0617@gmail.com>
2023-09-27 13:15:21 -0400

    implement styling to message input

--- client/src/components/ChatRoom/ChatRoom.jsx
+++ client/src/components/ChatRoom/ChatRoom.jsx
@@ -4,6 +4,8 @@ import { useContext, useEffect, useState } from "react";
 import { CurrentRoomContext } from "../Dashboard/Dashboard";
 import { CreateMessage, FindMessages } from "../../requests/messages";
 import { Button, FluidForm, TextInput, Theme } from "@carbon/react";
+import { Send } from "@carbon/icons-react";
+import Avvvatars from "avvvatars-react";
 
 const ChatRoom = () => {
     // user variable from firebase
@@ -34,26 +36,41 @@ const ChatRoom = () => {
     async function handleSubmit(e) {
         e.preventDefault();
         console.log(`handleSubmit: ${messageInput}`);
-        await CreateMessage(currentRoom.messageThread, user.email, messageInput);
+        if (messageInput != '') {
+            await CreateMessage(currentRoom.messageThread, user.email, messageInput);
+        }
         setMessageInput('');
     }
 
     return (
         <div className="chat-room">
             {currentRoom ? <h1>{currentRoom.name}</h1> : <h1>Choose a chat room to start messaging</h1>}
-            <div className="chat-room--messages"></div>
-            { currentRoom &&
-                <div className="chat-room--input" theme={"g90"}>
-                    <form onSubmit={handleSubmit}>
+            <div className="chat-room--messages">
+                {currentRoom && messages.map((message, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className={`chat-room--message-wrapper 
+                            ${user.email === message.senderEmail && " chat-room--message__current-user"}`}
+                        >
+                            {user.email !== message.senderEmail &&
+                                <Avvvatars value={message.senderEmail} size="40" />
+                            }
+                            <p className={"chat-room--message-bubble"}>{message.content}</p>
+                        </div>
+                    )
+
+                })}
+            </div>
+            {currentRoom &&
+                <div theme={"g90"}>
+                    <form className="chat-room--input" onSubmit={handleSubmit}>
                         <TextInput
                             id={"chat-room--text-input"}
-                            size={"xl"}
-                            labelText={"make a message"}
                             placeholder={"message"}
                             value={messageInput}
                             onChange={handleMessageInputChange}
                         />
-                        <Button type="submit">Submit</Button>
                     </form>
                 </div>
             }
