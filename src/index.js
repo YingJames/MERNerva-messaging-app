3a943d0a498d1f864e3805021707ec6735e97cf4
James Y <jyab0617@gmail.com>
2023-10-02 22:28:45 -0400

    implement netlify functions

--- src/index.js
+++ src/index.js
@@ -1,60 +1,77 @@
 import "./loadEnvironment.mjs";
-import express, { json } from "express";
+import express, { json, Router } from "express";
+import serverless from "serverless-http";
 import cors from "cors";
 import path from "path";
-import { fileURLToPath } from "url";
-import { connectToMongoClient } from "./config/mongoClient.mjs";
-import { messageRoutes, roomRoutes, userRoutes } from "./routes/index.mjs";
-import colors from "colors";
-import MessageThread from "./models/messageThread.mjs";
-import { Room } from "./models/room.mjs";
+// import { fileURLToPath } from "url";
+// import { connectToMongoClient } from "./config/mongoClient.mjs";
+// import { messageRoutes, roomRoutes, userRoutes } from "./routes/index.mjs";
+// import colors from "colors";
+// import MessageThread from "./models/messageThread.mjs";
+// import { Room } from "./models/room.mjs";
+// require("./loadEnvironment.mjs");
+// const express = require("express");
+// const serverless = require("serverless-http");
+// const cors = require("cors");
+// const path = require("path");
+// const { fileURLToPath } = require("url");
+// const { connectToMongoClient } = require("./config/mongoClient.mjs");
+// const { messageRoutes, roomRoutes, userRoutes } = require("./routes/index.mjs");
+// const colors = require("colors");
+// const MessageThread = require("./models/messageThread.mjs");
+// const { room } = require("./models/room.mjs");
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+const netlifyRouter = Router();
 
 app.use(cors());
 app.use(json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-connectToMongoClient();
+// connectToMongoClient();
 
-app.get('/watchRooms', (req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-    });
-    const changeStream = Room.watch();
-    changeStream.on('change', () => {
-        const data = { message: "rerender" }
-        res.write("event: message\n");
-        res.write(`data: ${JSON.stringify(data)}\n\n`);
-    });
-});
-app.get('/watchMessageThread', (req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-    });
-    const changeStream = MessageThread.watch();
-    changeStream.on('change', () => {
-        const data = { message: "rerender" }
-        res.write("event: message\n");
-        res.write(`data: ${JSON.stringify(data)}\n\n`);
-    });
-});
+// netlifyRouter.get('/watchRooms', (req, res) => {
+//     res.writeHead(200, {
+//         "Content-Type": "text/event-stream",
+//         "Cache-Control": "no-cache",
+//         "Connection": "keep-alive",
+//     });
+//     const changeStream = Room.watch();
+//     changeStream.on('change', () => {
+//         const data = { message: "rerender" }
+//         res.write("event: message\n");
+//         res.write(`data: ${JSON.stringify(data)}\n\n`);
+//     });
+// });
+// netlifyRouter.get('/watchMessageThread', (req, res) => {
+//     res.writeHead(200, {
+//         "Content-Type": "text/event-stream",
+//         "Cache-Control": "no-cache",
+//         "Connection": "keep-alive",
+//     });
+//     const changeStream = MessageThread.watch();
+//     changeStream.on('change', () => {
+//         const data = { message: "rerender" }
+//         res.write("event: message\n");
+//         res.write(`data: ${JSON.stringify(data)}\n\n`);
+//     });
+// });
 
-app.use("/api/database/users", userRoutes);
-app.use("/api/database/rooms", roomRoutes);
-app.use("/api/database/messages", messageRoutes);
+// app.use("/.netlify/functions/api/database/users", userRoutes);
+// app.use("/.netlify/functions/api/database/rooms", roomRoutes);
+// app.use("/.netlify/functions/api/database/messages", messageRoutes);
 
+/*
 app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (request, response) => {
+netlifyRouter.get('*', (request, response) => {
     response.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+*/
+
+app.use('/api/', netlifyRouter);
 
 // replace console.* for disable log on production
 if (process.env.NODE_ENV === 'production') {
@@ -63,6 +80,7 @@ if (process.env.NODE_ENV === 'production') {
   console.debug = () => {}
 }
 
-app.listen(PORT, () => {
-    console.log(colors.yellow.bold(`Server is running on http://localhost:${PORT}`));
-});
+export const handler = serverless(app);
+// app.listen(PORT, () => {
+//     console.log(colors.yellow.bold(`Server is running on http://localhost:${PORT}`));
+// });
